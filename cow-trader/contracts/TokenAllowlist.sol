112d76e514b822137447464d8457017f89b58c28
lumoswiz <lumoswiz@protonmail.com>
2025-02-05 23:38:23 +0100

    feat: add isOrderAllowed view function

--- cow-trader/contracts/TokenAllowlist.sol
+++ cow-trader/contracts/TokenAllowlist.sol
@@ -7,37 +7,41 @@ import {EnumerableSetLib} from "solady/src/utils/EnumerableSetLib.sol";
 contract TokenAllowlist is Ownable {
     using EnumerableSetLib for EnumerableSetLib.AddressSet;
 
-    EnumerableSetLib.AddressSet internal allowList;
+    EnumerableSetLib.AddressSet internal allowlist;
 
     constructor() {
         _initializeOwner(msg.sender);
     }
 
     function addToken(address token) external onlyOwner {
-        allowList.add(token);
+        allowlist.add(token);
     }
 
     function addTokensBatch(address[] memory tokens) external onlyOwner {
         for (uint256 i; i < tokens.length; ++i) {
-            allowList.add(tokens[i]);
+            allowlist.add(tokens[i]);
         }
     }
 
     function removeToken(address token) external onlyOwner {
-        allowList.remove(token);
+        allowlist.remove(token);
     }
 
     function removeTokensBatch(address[] memory tokens) external onlyOwner {
         for (uint256 i; i < tokens.length; ++i) {
-            allowList.remove(tokens[i]);
+            allowlist.remove(tokens[i]);
         }
     }
 
     function allowedTokens() external view returns (address[] memory) {
-        return allowList.values();
+        return allowlist.values();
     }
 
-    function isAllowed(address token) external view returns (bool) {
-        return allowList.contains(token);
+    function isAllowed(address token) public view returns (bool) {
+        return allowlist.contains(token);
+    }
+
+    function isOrderAllowed(address sellToken, address buyToken) external view returns (bool) {
+        return isAllowed(sellToken) && isAllowed(buyToken);
     }
 }
