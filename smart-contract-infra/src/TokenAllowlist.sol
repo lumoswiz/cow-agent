4a7f1f9fce9a7c33983d548f439d7e5bedc57202
lumoswiz <lumoswiz@protonmail.com>
2025-02-06 23:42:45 +0100

    feat: add TokenAllowlist to Foundry project

---
+++ smart-contract-infra/src/TokenAllowlist.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import { Ownable } from "solady/src/auth/Ownable.sol";
+import { EnumerableSetLib } from "solady/src/utils/EnumerableSetLib.sol";
+
+contract TokenAllowlist is Ownable {
+    using EnumerableSetLib for EnumerableSetLib.AddressSet;
+
+    EnumerableSetLib.AddressSet internal allowlist;
+
+    constructor() {
+        _initializeOwner(msg.sender);
+    }
+
+    function addToken(address token) external onlyOwner {
+        allowlist.add(token);
+    }
+
+    function addTokensBatch(address[] memory tokens) external onlyOwner {
+        for (uint256 i; i < tokens.length; ++i) {
+            allowlist.add(tokens[i]);
+        }
+    }
+
+    function removeToken(address token) external onlyOwner {
+        allowlist.remove(token);
+    }
+
+    function removeTokensBatch(address[] memory tokens) external onlyOwner {
+        for (uint256 i; i < tokens.length; ++i) {
+            allowlist.remove(tokens[i]);
+        }
+    }
+
+    function allowedTokens() external view returns (address[] memory) {
+        return allowlist.values();
+    }
+
+    function isAllowed(address token) public view returns (bool) {
+        return allowlist.contains(token);
+    }
+
+    function isOrderAllowed(address sellToken, address buyToken) external view returns (bool) {
+        return isAllowed(sellToken) && isAllowed(buyToken);
+    }
+}
