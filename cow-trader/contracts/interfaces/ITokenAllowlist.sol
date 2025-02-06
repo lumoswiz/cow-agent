36af254865415739d1c7bb8316ed1dce8985a4d2
lumoswiz <lumoswiz@protonmail.com>
2025-02-06 23:39:16 +0100

    refactor: remove smart contracts + tests from cow-trade/

--- cow-trader/contracts/interfaces/ITokenAllowlist.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.25;
-
-interface ITokenAllowlist {
-    function isOrderAllowed(address sellToken, address buyToken) external view returns (bool);
-}
