5e2c38de67019b15f77591d1a6ba8dc284d24cd4
lumoswiz <lumoswiz@protonmail.com>
2025-02-07 18:53:18 +0100

    docs: add natspect

--- smart-contract-infra/src/CoWSwapGuard.sol
+++ smart-contract-infra/src/CoWSwapGuard.sol
@@ -5,13 +5,20 @@ import { Enum } from "@gnosis.pm/safe-contracts/contracts/common/Enum.sol";
 import { BaseGuard } from "@gnosis-guild/zodiac/contracts/guard/BaseGuard.sol";
 
 contract CoWSwapGuard is BaseGuard {
+    /// @notice Thrown when the target address is not GPv2Settlement contract address
     error InvalidAddress();
+
+    /// @notice Thrown when the function selector is not `setPreSignature(bytes,bool)`
     error InvalidSelector();
 
+    /// @notice GPv2Settlement address
+    /// @dev Deterministically deployed
     address internal constant GPV2_SETTLEMENT_ADDRESS = 0x9008D19f58AAbD9eD0D60971565AA8510560ab41;
 
+    /// @notice Function selector for GPv2Settlement.setPreSignature
     bytes4 internal constant SET_PRE_SIGNATURE_SELECTOR = 0xec6cb13f;
 
+    /// @dev Module transactions only use the first four parameters: to, value, data, and operation.
     function checkTransaction(
         address to,
         uint256,
