d21145dcccc2e57cd7828288ead889ff33740084
lumoswiz <lumoswiz@protonmail.com>
2025-02-12 23:41:54 +0100

    feat: add tokens to allowlist script

--- smart-contract-infra/script/ScriptUtils.sol
+++ smart-contract-infra/script/ScriptUtils.sol
@@ -15,6 +15,10 @@ contract ScriptUtils is Script {
     address internal constant MODULE_PROXY_FACTORY = 0x000000000000aDdB49795b0f9bA5BC298cDda236;
     address internal constant ZERO_ADDRESS = address(0);
 
+    address internal constant GNO = 0x9C58BAcC331c9aa871AFD802DB6379a98e80CEdb;
+    address internal constant COW = 0x177127622c4A00F3d409B75571e12cB3c8973d3c;
+    address internal constant WXDAI = 0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d;
+
     function _writeDeploymentAddress(address addr, string memory key) internal {
         string memory root = vm.projectRoot();
         string memory path = string.concat(root, "/deployments/contracts.json");
